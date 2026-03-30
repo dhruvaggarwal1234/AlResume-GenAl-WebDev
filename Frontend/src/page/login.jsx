d51b7bcfd6cc6c763a5a509bc527793304348acf
dhruvaggarwal1234 <Dhruvaggarwal755@gmail.com>
2026-03-30 23:15:39 +0530

    git

---
+++ Frontend/src/page/login.jsx
@@ -0,0 +1,19 @@
+function Login(){
+ return(
+
+    <>
+    
+    <form action="/submit">
+    
+    <label htmlFor="email">Email</label>
+    <input type="text"  />
+
+    <label htmlFor="email">Password</label>
+    <input type="text" />
+
+    </form>
+
+    </>
+ );
+}
+
